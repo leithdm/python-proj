da3d8c870e8c8090e153125a61c4f94a0138eaa8
leithdm@gmail.com <leithdm@gmail.com>
2021-01-17 13:50:14 +0000

    feat: Add day4 soln

---
+++ projects/day-4/day4.py
@@ -0,0 +1,89 @@
+import random
+import my_module
+
+'''
+random_integer = random.randint(1, 10)
+print(random_integer)
+# print(my_module.pi)
+random_float = random.random() * 5
+print(random_float) 
+
+print('💩')
+
+
+
+# 🚨 Don't change the code below 👇
+row1 = ["⬜","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+int_column = int(position[0])
+int_row = int(position[1])
+map[int_row - 1][int_column - 1] = 'x'
+
+
+
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
+
+'''
+
+# rock, paper, scissors
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if user_choice >= 3 or user_choice < 0: 
+  print("You typed an invalid number, you lose!") 
+else:
+    print(game_images[user_choice])
+
+    computer_choice = random.randint(0, 2)
+    print("Computer chose:")
+    print(game_images[computer_choice])
+    
+    if user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose")
+    elif computer_choice > user_choice:
+        print("You lose")
+    elif user_choice > computer_choice:
+        print("You win!")
+    elif computer_choice == user_choice:
+        print("It's a draw")
