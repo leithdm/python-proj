212143290a55710348e71b41031461659b017865
leithdm@gmail.com <leithdm@gmail.com>
2021-01-25 12:19:52 +0000

    feat: Add soln for day22

---
+++ projects/day-22/main.py
@@ -0,0 +1,53 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 300 or ball.ycor() < -300:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        scoreboard.l_point()
+        scoreboard.update_scoreboard()
+        ball.reset_position()
+
+    # Detect L paddle misses
+    if ball.xcor() < -380:
+        scoreboard.r_point()
+        scoreboard.update_scoreboard()
+        ball.reset_position()
+
+screen.exitonclick()
