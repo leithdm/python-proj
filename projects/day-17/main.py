d1832dd28f078cebea02fd23b724c546c89ccd7b
leithdm@gmail.com <leithdm@gmail.com>
2021-02-09 16:56:39 +0000

    feat: Add missing files for day-17

--- projects/day-17/main.py
+++ projects/day-17/main.py
@@ -1,16 +1,18 @@
 from question_model import Question
 from data import question_data
-from question_model import Question
 from quiz_brain import QuizBrain
 
 question_bank = []
-for data in question_data:
-    question_bank.append(Question(data["text"], data["answer"]))
-
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
 while quiz.still_has_questions():
     quiz.next_question()
-print("You have completed the quiz")
-print(f"You're final score was {quiz.score} / {len(question_bank)}")
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
